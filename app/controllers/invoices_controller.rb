1111af368102da68b111c0958f4f7726a7d640f8
emilioriosvz <emilioriosvz@gmail.com>
2015-01-18 18:35:17 +0100

    Invoices views

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,39 @@
+class InvoicesController < ApplicationController
+  def index
+    @invoices = Invoice.all
+  end
+
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save!
+      redirect_to invoices_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def edit
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def destroy
+    @invoice = Invoice.find(params[:id])
+    @invoice.destroy!
+    redirect_to invoices_path
+  end
+
+  private
+    def invoice_params
+      params.require(:invoice).permit(:invoice_date, :client)
+    end
+
+end
--- app/models/articles.rb
+++ app/models/articles.rb
@@ -1,3 +1,3 @@
 class Article < ActiveRecord::Base
-  has_and_belong_to_many :invoice
+  # has_and_belongs_to_many :invoice
 end
